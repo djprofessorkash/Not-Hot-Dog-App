bf299b10e92ec6d83e58c1e1344f16df6de590ac
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2023-05-04 12:52:02 -0400

    [EDIT] Built single image loading method

--- research/ingestion.py
+++ research/ingestion.py
@@ -5,14 +5,35 @@ INTENT: The objective of this file is to produce a basic functional data scraper
         ImageNet parent dataset. 
 """
 
+print("Importing libraries...")
+import itertools, os, urllib, cv2
+# import numpy as np
+from multiprocessing.dummy import Pool as ThreadPool
+print("Libraries successfully imported.")
+
+
 class IngestionEngine(object):
     """ Standalone class for ingesting scraped images as data. """
     def __init__(self):
-        # print("Importing libraries...")
-        import itertools, os, urllib, cv2
-        import numpy as np
-        from multiprocessing.dummy import Pool as ThreadPool
-        # print("Libraries successfully imported.")
+        self.DATASIZE = 1
+
+    def load_single_image(self, path, link, counter):
+        if self.DATASIZE < counter:
+            self.DATASIZE = counter + 1
+        try:
+            full_path_to_image = f"{path}/{str(counter)}.jpg"
+            urllib.request.urlretrieve(link,full_path_to_image)
+            image = cv2.imread(full_path_to_image)
+            if image is not None:
+                cv2.imwrite(full_path_to_image, image)
+                print(f"Downloaded image `{str(counter)}.jpg`.")
+        except Exception as e:
+            print(str(e))
+            print("Failed to download image.")
+            return False
+
+    def load_all_images(paths, links):
+        pass
 
 if __name__ == "__main__":
     pipeline = IngestionEngine()
