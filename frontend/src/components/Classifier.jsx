1a19f0fb98278bbaaa9a1f2a3111a318c9cc6b85
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2023-05-12 11:04:02 -0400

    [ADD] Pushing React subdirectory with boilerplate setup

---
+++ frontend/src/components/Classifier.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useRef, useState } from "react";
+
+const Classifier = () => {
+    const canvasRef = useRef();
+    const imageRef = useRef();
+    const videoRef = useRef();
+
+    const [result, setResult] = useState("");
+
+    useEffect(() => {
+        // TODO: Fetch/get camera feed here.
+    }, []);
+
+    useEffect(() => {
+        // TODO: Send images to API here.
+    }, []);
+
+    return (
+        <>
+            <h1>Image Classifier</h1>
+            <div></div>
+        </>
+    )
+};
+
+export default Classifier;
