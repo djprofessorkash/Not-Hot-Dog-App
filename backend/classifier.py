1d754c65ed7153cedf89de0cbbee0b25d591d127
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2023-05-12 11:11:41 -0400

    [EDIT] Reconstructed classifier with OO architecture for modularity

--- backend/classifier.py
+++ backend/classifier.py
@@ -2,34 +2,27 @@ import os
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
 from PIL import ImageFile, Image
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 from numpy import expand_dims
 from werkzeug.utils import secure_filename
 from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
 from tensorflow.keras.models import load_model
-# from tensorflow.keras.Model import load_weights
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.inception_v3 import InceptionV3
-ImageFile.LOAD_TRUNCATED_IMAGES = True
 
-# model = InceptionV3(weights="imagenet", include_top=False)
-PATH_TO_CLASSIFIER = "../research/models/weights_inception.hdf5"
-classifier = load_model(PATH_TO_CLASSIFIER)
+class ImageClassifier(object):
+    def __init__(self):
+        self.PATH_TO_CLASSIFIER = "../research/models/weights_inception.hdf5"
+        self.classifier = load_model(self.PATH_TO_CLASSIFIER)
 
-def get_prediction(image_as_bytes, model):
-    raw_image = Image.open(image_as_bytes)
-    converted_image = raw_image.convert("RGB").resize((299, 299), Image.NEAREST)
-
-    image_tensor = image.img_to_array(converted_image)
-    batched_tensor = expand_dims(image_tensor, axis=0)
-
-    processed_tensor = preprocess_input(batched_tensor, mode="caffe")
+    def predict_probabilities(self, image_as_bytes):
+        raw_image = Image.open(image_as_bytes)
+        converted_image = raw_image.convert("RGB").resize((299, 299), Image.NEAREST)
+        image_tensor = image.img_to_array(converted_image)
+        batched_tensor = expand_dims(image_tensor, axis=0)
+        processed_tensor = preprocess_input(batched_tensor, mode="caffe")
+        return self.classifier.predict(processed_tensor)
     
-    return model.predict(processed_tensor)
-
-
-def classify_image(image_file):
-    predicted_probabilities = get_prediction(image_file, classifier)
-
-    prediction_tensor = decode_predictions(predicted_probabilities, top=1)
-    
-    return str(prediction_tensor[0][0][1])
+    def classify_image(self, image_file):
+        self.predicted_probabilities = self.predict_probabilities(image_file)
+        self.binary_classification = decode_predictions(self.predicted_probabilities, top=1)[0][0][1]
